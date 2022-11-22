517ba5a4d27fd58ae4617cb3d1b285619d42f872
senivlm <91743906+senivlm@users.noreply.github.com>
2022-11-22 17:05:15 +0200

    Update Program.cs

--- Homework_6_Kasianenko_Viacheslav/HomeWork6_Task1/HomeWork6_Task1/Program.cs
+++ Homework_6_Kasianenko_Viacheslav/HomeWork6_Task1/HomeWork6_Task1/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             AccountingEletricity accountingEletricity = new AccountingEletricity(3);
+            //чому рік стрічкою?
             accountingEletricity.ReadFile(1, "2021");
             accountingEletricity.WriteFileInfoForUser();
             accountingEletricity.WriteFileAboutOneApartament(1);
