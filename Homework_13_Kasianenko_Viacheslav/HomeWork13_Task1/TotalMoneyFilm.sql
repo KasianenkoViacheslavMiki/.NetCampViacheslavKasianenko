8cf1e1e6b8f52ce9176cf7646e17e2ce847fdbb4
Kasianenko Viacheslav <55177392+KasianenkoViacheslavMiki@users.noreply.github.com>
2023-01-23 06:05:18 +0200

    HomeWork13: Task v1.02

--- Homework_13_Kasianenko_Viacheslav/HomeWork13_Task1/TotalMoneyFilm.sql
+++ Homework_13_Kasianenko_Viacheslav/HomeWork13_Task1/TotalMoneyFilm.sql
@@ -1,7 +1,7 @@
 use CinemaSigmaUA
 go
 
-SELECT Films.NameFilm, SUM(Showtimes.PriceShowTime) AS Total
+SELECT Films.NameFilm, SUM(Showtimes.PriceShowTime) * COUNT(Bookings.GuidBooking) AS Total
 FROM Films INNER JOIN
            Showtimes ON Films.GuidFilm = Showtimes.GuidFilm INNER JOIN
            Bookings ON Showtimes.GuidShowtimes = Bookings.GuidShowtimes
