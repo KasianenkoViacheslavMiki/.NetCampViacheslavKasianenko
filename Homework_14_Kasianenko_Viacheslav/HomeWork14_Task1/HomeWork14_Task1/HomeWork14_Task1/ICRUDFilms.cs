686c72363462b27164cf3549c1f51cf865b49e8b
Kasianenko Viacheslav <55177392+KasianenkoViacheslavMiki@users.noreply.github.com>
2023-01-31 20:43:37 +0200

    HomeWork 14: Fix interface

--- Homework_14_Kasianenko_Viacheslav/HomeWork14_Task1/HomeWork14_Task1/HomeWork14_Task1/ICRUDFilms.cs
+++ Homework_14_Kasianenko_Viacheslav/HomeWork14_Task1/HomeWork14_Task1/HomeWork14_Task1/ICRUDFilms.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HomeWork15_Task1
+namespace HomeWork14_Task1
 {
     public interface ICRUDFilms
     {
         public Task CreateFilm(Film film);
-        public Task<Films> ReadFilmByName(string nameFilm);
-        public Task<List<Films>> ReadFilms();
+        public Task<Film> ReadFilmByName(string nameFilm);
+        public Task<List<Film>> ReadFilms();
         public Task UpdateFilm(string nameChangeFilm,Film film);
         public Task DeleteFilm(Film film);
     }
